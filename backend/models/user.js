fc2fc6666d9a35eb2c6bbfdc2e2d80033246852d
ANAGHA-A-04 <anagha8848@gmail.com>
2026-02-27 01:03:13 +0530

    error correctiom

--- backend/models/user.js
+++ backend/models/user.js
@@ -40,6 +40,18 @@ const userschema = new mongoose.Schema ({
         enum:["lose weight","gain muscle","maintain fitness"],
 
     }
-    userSchema
+    userSchema.pre('save',async function(next){
+        if(!this.isModified('password')){
+            return next();
+        }   
+        try{
+            const salt = await bcrypt.genSalt(10);
+            this.password = await bcrypt.hash(this.password,salt);
+            next();
+        }        catch(err){
+            next(err);
+        }
+
 
 })
+module.exports=mongoose.model('User',userschema);
