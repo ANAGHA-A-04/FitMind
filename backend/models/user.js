14cc52d4849ff3d0a5e27c8f7d9d748676cce90f
ANAGHA-A-04 <anagha8848@gmail.com>
2026-02-27 00:56:43 +0530

    user model commited

---
+++ backend/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require ('mongoose');
+const bcrypt =require('bcrypt');
+const userschema = new mongoose.Schema ({
+    name:{
+        required:[true,"Name is required"],
+        type:String,
+        
+    },
+    password:{
+        required:[true,"password is required"],
+        type:String,
+        minLength:[6,"'password must be at least 6 characters"],
+        select:false,
+        
+
+    },
+    email:{
+        type:String,
+        required:[true,"email is required"],
+        unique:true,
+        trim:true,
+        lowercase:true,
+        match:[/^\S+@\S+\.\S+$/, "Please enter a valid email"],
+    },
+    weight:{
+        type:Number,
+        required:[true,"weight is required"],
+    },
+    height:{
+        type:Number,
+        required:[true,"height is required"],
+    },
+    dateOfBirth:{
+        type:Date,
+        required:[true,"date of birth is required"],
+    },
+    goal:{
+        type:String,
+        required:[true,"goal is required"],
+        enum:["lose weight","gain muscle","maintain fitness"],
+
+    }
+    userSchema
+
+})
